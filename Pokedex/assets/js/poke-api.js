5d3bcf827c669b70e962d6f5b9276261637abaa7
Pedro Henrique G Rezende <94559555+pedrorezende07@users.noreply.github.com>
2023-07-21 07:24:39 -0300

    Melhorias no CSS e leitura da API

--- Pokedex/assets/js/poke-api.js
+++ Pokedex/assets/js/poke-api.js
@@ -2,7 +2,7 @@
 
 const pokeApi = {}
 
-pokeApi.getPokemons = (offset = 0, limit = 10) => { //arrow function com parametros e default, caso nao recebar nos parametros
+pokeApi.getPokemons = (offset = 0, limit = 8) => { //arrow function com parametros e default, caso nao recebar nos parametros
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
     
     return fetch(url)
@@ -10,11 +10,29 @@ pokeApi.getPokemons = (offset = 0, limit = 10) => { //arrow function com paramet
         .then((jsonBody) => jsonBody.results) //pega o json e entra na area de resultados
         .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail)) //mapeando todos os url do primeiro json
         .then((detailRequests) => Promise.all(detailRequests)) //promise.all aguardando todos a leituras de url
-        .then((pokemonsDetails) => pokemonsDetails) //e retortando ao getPokemons os detalhes dos pokemons
+        .then((pokemonsDetails) => pokemonsDetails) //e retornando ao getPokemons os detalhes dos pokemons
         
         .catch((error) => console.error(error))
 }
 
+function convertPokeApiDetailToPokemon(pokeDetail){
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.order
+    pokemon.name = pokeDetail.name
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+}
+
 pokeApi.getPokemonDetail = (pokemon) => {
-    return fetch(pokemon.url).then((response) => response.json()) //recebe o body do primeiro json e mapea todas as url
+    return fetch(pokemon.url)
+        .then((response) => response.json()) //recebe o body do primeiro json e mapea todas as url
+        .then(convertPokeApiDetailToPokemon)
 }
